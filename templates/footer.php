78b0be6a004bc7b4e4d5d7e1d989701be7c0a8a4
irene.delatorre <delatorr@mit.edu>
2018-01-12 13:48:26 -0500

    added analytics code to footer

--- templates/footer.php
+++ templates/footer.php
@@ -67,6 +67,7 @@
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-109416268-1"></script>
 <script>
     $(".parallax-window").each(function() {
         var background_image_src = $(this).data("image-src");
