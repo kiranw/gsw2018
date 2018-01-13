9d6fbce33224cd3bda9ad51947d502c81d881ae8
irene.delatorre <delatorr@mit.edu>
2018-01-13 14:52:31 -0500

    worked on the startup-showcase page, we still need a slogan

--- startup-showcase.php
+++ startup-showcase.php
@@ -17,27 +17,34 @@
                 </div>
                 <div class="col-md-1"></div>
                 <div class="col-md-9 marginTop">
-                    <p>
-                        The MIT GSW Startup Showcase offers the opportunity to:
-                    </p>
-
+                    <p class="date">26 March Monday</p>
+                    <p class="competition-slogan">Write nice slogan phrase</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="competition-subtitle">Format</h2>
+                    <p>The MIT GSW Startup Showcase offers the opportunity to:</p>
                     <ul>
-                        <li>Display your products/services to over 750 attendees at GSW</li>
-                        <li>Meet with potential investors, mentors, and government officials</li>
-                        <li>Participate in a networking night for startups</li>
+                        <li class="list-competition"><span style="font-weight: 800">Display your products/services</span> to over 750 attendees at GSW</li>
+                        <li class="list-competition">Meet with potential <span style="font-weight: 800">investors, mentors, and government officials</span></li>
+                        <li class="list-competition">Participate in a <span style="font-weight: 800">networking night</span> for startups</li>
                     </ul>
-
+                    <br />
+                </div>
+                <div class="col-md-6">
+                    <h2 class="competition-subtitle">Eligibility</h2>
+                    <p>Who should apply?</p>
+                    <br>
+                    <ul>
+                        <li class="list-competition">All early stage startups looking to advertise their company, find potential investors, and seek advice</li>
+                        <li class="list-competition">Startups must have less than $250,000 USD in annual revenue to be eligible</li>
+                    </ul>
                     <br />
                 </div>
             </div>
-            <h2>Who should apply?</h2>
-            <ul>
-              <li>All early stage startups looking to advertise their company, find potential investors, and seek advice</li>
-              <li>Startups must have less than $250,000 USD in annual revenue to be eligible</li>
-            </ul>
-            <br />
             <div class="text-center">
-                <a class="btn btn-default register-button" href="#" role="button">Apply now to Startup Showcase</a>
+                <a class="btn btn-default register-button" href="#" role="button">Apply now<br>to Startup Showcase</a>
             </div>
         </div>
     </div>
