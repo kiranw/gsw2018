bfb05c280df55cdbafb594389fa6e41cbbc6b3fd
Gabriela <gabrielascardine@gmail.com>
2018-01-14 15:19:00 -0500

    Small adjustments

--- templates/nav-bar.php
+++ templates/nav-bar.php
@@ -43,8 +43,6 @@
                         <a class="dropdown-item" href="bpc.php">Business Plan Competition</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="startup-showcase.php">Startup Showcase</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="mentors.php">Our mentors</a>
                     </div>
                 </li>
             </ul>
